7ef978eb5b0e6a82b6897d714ee977a2cedd5ea6
Yos Riady <yosriady@gmail.com>
2014-10-12 13:38:10 +0800

    File output

--- readfile.rb
+++ readfile.rb
@@ -12,13 +12,13 @@ end
 
 File.open(ARGV[1], "r") do |file|
   line = file.gets
-  # results = p.translate_virtual_addresses(line)
-  # p results.join(" ")
+  results = p.translate_virtual_addresses(line)
+  p results.join(" ")
 
-  results2 = p.translate_virtual_addresses_with_lru(line)
-  p results2.join(" ")
+  # results2 = p.translate_virtual_addresses_with_lru(line)
+  # p results2.join(" ")
 
-  # destination_path = File.dirname(input_path)+ "/A0099317U.txt"
-  # File.open(destination_path, 'a') { |file| file.write(results.join(" "))}
+  destination_path = File.dirname(file.path)+ "/A0099317U.txt"
+  File.open(destination_path, 'w') { |file| file.write(results.join(" "))}
   # File.open(destination_path, 'a') { |file| file.write(results2.join(" "))}
 end
